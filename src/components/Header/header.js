f3adc6533afc1f475d82656e7aeee24286d0dc7b
jennie <jennie.tompowsky@hotmail.com>
2023-10-25 21:45:47 +0200

    added code to navsection and css

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -3,12 +3,10 @@ import React from 'react'
 import Navsection from './navsection/navsection'
 import Showcase from './showcase/showcase'
 
-import img_critologotype from '../../assets/images/logotype.svg'
-
 const header = () => {
   return (
     <header>
-            <img className="logotype.svg" src={img_critologotype} alt="Crito Logotype" />
+            
         <Navsection />
         <Showcase />
     </header>
