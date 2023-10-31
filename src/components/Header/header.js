5e1834674280be2dfedce7beff4f64216566d2f5
jennie <jennie.tompowsky@hotmail.com>
2023-10-31 12:50:19 +0100

    changed to navlink and link in header section

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,5 +1,6 @@
 import './header.css'
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
 import img_critologotype from '../../assets/images/logotype.svg'
 
@@ -7,7 +8,7 @@ const header = () => {
   return (
     <header>
     <div className="container">
-        <img className="logotype.svg" src={img_critologotype} alt="Crito Logotype" />
+        <Link to="/" ><img className="logotype.svg" src={img_critologotype} alt="Crito Logotype" /></Link>
         <div className="menu-bar"><i className="fa-regular fa-bars-staggered"></i></div>
         <div className="menu">
             <div className="contact-menu">
@@ -26,20 +27,20 @@ const header = () => {
                     </div>
                 </div>
                 <div className="social-media">
-                    <a href="http://www.facebook.com" target="_blank"><i className="fa-brands fa-facebook"></i></a>
-                    <a href="http://www.twitter.com" target="_blank"><i className="fa-brands fa-twitter"></i></a>
-                    <a href="http://www.instagram.com" target="_blank"><i className="fa-brands fa-instagram"></i></a>
-                    <a href="http://www.linkedin.com" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
+                    <Link to="http://www.facebook.com" target="_blank"><i className="fa-brands fa-facebook"></i></Link>
+                    <Link to="http://www.twitter.com" target="_blank"><i className="fa-brands fa-twitter"></i></Link>
+                    <Link to="http://www.instagram.com" target="_blank"><i className="fa-brands fa-instagram"></i></Link>
+                    <Link to="http://www.linkedin.com" target="_blank"><i className="fa-brands fa-linkedin"></i></Link>
                 </div>
             </div>
             <div className="main-menu">
                 <nav>
-                    <a className="active" href="/">Home</a>
-                    <a href="service.html">Service</a>
-                    <a href="news.html">News</a>
-                    <a href="/contact">Contact</a>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="service.html">Service</NavLink>
+                    <NavLink to="news.html">News</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
                 </nav>
-                <a className="btn btn-yellow">Login <i className="fa-light fa-arrow-up-right"></i></a>
+                <Link to="/login" className="btn btn-yellow">Login <i className="fa-light fa-arrow-up-right"></i></Link>
             </div>
         </div>
     </div>
